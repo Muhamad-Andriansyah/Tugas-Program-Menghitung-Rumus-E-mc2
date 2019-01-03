35c9fce8cfebf5c61d5c4989133a336438cc37de
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 11:31:58 +0700

    Add files via upload

---
+++ Tugas Program Menghitung Rumus E=mc2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MENGHITUNG RUMUS E=mc2 - MUHAMAD ANDRIANSYAH (NIM 311810297) TI-18 B2" << endl;
+
+        int C,M, E;
+
+        cout<<"Masukan C:";
+        cin>>C;
+        cout<<"Masukan M:";
+        cin>>M;
+        E=(C * M)*(C * M);
+        cout<<"masukan kuadrat CM adalah"<<E;
+
+    return 0;
+
+}
